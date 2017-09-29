63258f259a67742a23885fd4d811b1fc33bf791d
Jonas Claeson <jonas.cl@protonmail.com>
2017-09-29 15:56:12 +0200

    Break out the spiral generation to a separate file

---
+++ DoubleSpiral.hs
@@ -0,0 +1,23 @@
+module DoubleSpiral
+    ( runDoubleSpiral
+    ) where
+
+import Graphics.Implicit
+import Graphics.Implicit.Definitions
+
+gr = (1 + sqrt 5)/2
+
+createIntersection :: ℝ -> SymbolicObj2
+createIntersection r =
+    unionR (r/2/gr)
+        [ translate (-r, 0) $ circle r
+        , translate (r, 0) $ circle r
+        ]
+
+createSpiral :: ℝ -> SymbolicObj3
+createSpiral l = extrudeRM 0 (Just id) Nothing Nothing (createIntersection l) (Left 360)
+
+runDoubleSpiral :: IO ()
+runDoubleSpiral = do
+    writeSVG 2 "helloworld.svg" $ createIntersection 30
+    writeSTL 1 "helloworld.stl" $ createSpiral 30
--- Main.hs
+++ Main.hs
@@ -1,21 +1,7 @@
 module Main where
 
-import Graphics.Implicit
-import Graphics.Implicit.Definitions
-
-gr = (1 + sqrt 5)/2
-
-createIntersection :: ℝ -> SymbolicObj2
-createIntersection r =
-    unionR (r/2/gr)
-        [ translate (-r, 0) $ circle r
-        , translate (r, 0) $ circle r
-        ]
-
-createSpiral :: ℝ -> SymbolicObj3
-createSpiral l = extrudeRM 0 (Just id) Nothing Nothing (createIntersection l) (Left 360)
+import DoubleSpiral
 
 main :: IO ()
-main = do
-    writeSVG 2 "helloworld.svg" $ createIntersection 30
-    writeSTL 1 "helloworld.stl" $ createSpiral 30
+main =
+    runDoubleSpiral
